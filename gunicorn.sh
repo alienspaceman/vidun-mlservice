aceda73633fd9238923aafb1e82932bbffd91782
Katerina Frid <catherinefrid96@gmail.com>
2021-06-11 11:51:12 +0300

    fix command

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./cloud_sql_proxy -dir=/cloudsql -instances=vidun-rus:europe-west1:dbvidun -credential_file=mlservice_key_gcp.json&
-gunicorn -c gunicorn.config.py run:app
+gunicorn --chdir app -c gunicorn.config.py run:app
