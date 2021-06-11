83afd754a0b2fe47e2547a3be3d240b0fa05552b
Katerina Frid <catherinefrid96@gmail.com>
2021-06-11 12:30:20 +0300

    try3

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./cloud_sql_proxy -dir=/cloudsql -instances=vidun-rus:europe-west1:dbvidun -credential_file=mlservice_key_gcp.json&
-gunicorn --chdir app run:app -w 4 --threads 4 -b 0.0.0.0:5555
+gunicorn run:app -w 4 --threads 4 -b 0.0.0.0:5555
